b0d72c2d4a88138478da1ed1ccf2a6cb083d76dc
Nick <nick@prpel.co.uk>
2023-12-02 23:57:02 +0000

    styling complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{css,html,js}"],
+  content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'light': '#ECC9C7',
+        'light-accent': '#D5AD46',
+        'main': '#6A858D',
+        'dark-accent': '#6F7082',
+        'dark': '#512D3D'
+      }
+    },
   },
   plugins: [],
 };
